ce1503a46ef417ea95350b1b2af0e6f19abdd242
vizchamz <visalchamuditha@gmail.com>
2021-04-03 20:50:28 +0530

    pkg name changed

---
+++ src/classes/Room.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class Room {
+    String roomName;
+
+    public Room(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+}
